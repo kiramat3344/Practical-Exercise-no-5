2a706d4e45579225cff75b1171c150f7f6c85301
kiramat3344 <kiramat0323@gmail.com>
2024-09-20 10:47:20 +0500

    exercise no 5

---
+++ script.js
@@ -0,0 +1,61 @@
+// // Initialize the array
+// let array = [1, 2, 3, 4, 5];
+
+// // Remove the first two elements using splice method
+// array.splice(0, 2);
+
+// // Add three new elements to the end of the array
+// array.push(6, 7, 8);
+
+// // Use a for loop to print the new array
+// for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+// no 2
+// const array = [1, 2, 3, 4, 5];
+
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index] % 2 === 0) {
+//         console.log(index);
+//     }
+// }
+
+// no 3
+// let array = [3, 4, 5];
+
+// // Add two new elements to the beginning of the array
+// array.unshift(1, 2);
+
+// // Remove the last element from the array
+// array.pop();
+
+// // Print the new array using a for loop
+// for (let index = 0; index < array.length; index++) {
+//     console.log(array[index]);
+// }
+// no 4
+// const array = [1, 2, 3, 4, 5];
+
+// for (let index = 1; index < array.length; index++) {
+//     if (array[index] > array[index - 1]) {
+//         console.log(array[index]);
+//     }
+}
+
+// no 5
+// let array = [1, 2, 3, 4, 5];
+
+// // Find the index of element 3
+// let index = array.indexOf(3);
+
+// // Remove all elements starting from that index
+// array.splice(index);
+
+// // Use a for loop to print the new array
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+
+
+
